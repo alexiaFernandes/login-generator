d21df4147cadd9f6da3f91e4129eef1f091d1cba
alexiaFernandes <alexia.fernandes@univ-tlse3.fr>
2017-11-22 22:39:21 +0100

    test Jean Durand accent

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -6,4 +6,11 @@ public class LoginGeneratorTest extends TestCase {
     public void testGenerateLoginForNomAndPrenom() throws Exception {
     }
 
+    public void testGenerateLoginForDurandPaulAccent() throws Exception {
+        String loginPaulDurand =
+                loginGenerator.generateLoginForNomAndPrenom("Dùrand", "Paul");
+        assertThat(loginPaulDurand, equalTo("PDUR"));
+
+    }
+
 }
