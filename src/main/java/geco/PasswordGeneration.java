bff9fbd874cfed8a6c68e299f24228baa8c0ee71
alexiaFernandes <alexia.fernandes@univ-tlse3.fr>
2017-11-29 16:21:55 +0100

    close #8

--- src/main/java/geco/PasswordGeneration.java
+++ src/main/java/geco/PasswordGeneration.java
@@ -3,6 +3,10 @@ package geco;
 public class PasswordGeneration {
 
 
+    /**
+     * Génère un mot de passe aléatoire
+     * @return mot de passe de caractères généré aléatoirement.
+     */
      public String getRandomPassword(){
          String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
          String pass = "";
